c7a646bad5ac0ecd43a0bfd0b77799e9a5746530
bnau <naubertrand@gmail.com>
2019-11-01 16:20:55 +0100

    Mutex for state

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ extern crate gtk;
 
 use gtk::prelude::*;
 
-use std::cell::RefCell;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 mod app_state;
 use app_state::{AppState, State};
@@ -20,13 +19,13 @@ fn main() {
 
     let gui = Arc::new(Gui::new());
 
-    let state = Arc::new(RefCell::new(State::new()));
+    let state = Arc::new(Mutex::new(State::new()));
     {
         let button = &gui.button_click;
         let gui = Arc::clone(&gui);
         let state = Arc::clone(&state);
         button.connect_enter_notify_event(move |_, _| {
-            let mut state = state.borrow_mut();
+            let mut state = state.lock().unwrap();
             state.update(AppState::CLICK);
             gui.update_from(&state);
             Inhibit(false)
@@ -37,7 +36,7 @@ fn main() {
         let gui = Arc::clone(&gui);
         let state = Arc::clone(&state);
         button.connect_enter_notify_event(move |_, _| {
-            let mut state = state.borrow_mut();
+            let mut state = state.lock().unwrap();
             state.update(AppState::DROIT);
             gui.update_from(&state);
             Inhibit(false)
@@ -48,7 +47,7 @@ fn main() {
         let gui = Arc::clone(&gui);
         let state = Arc::clone(&state);
         button.connect_enter_notify_event(move |_, _| {
-            let mut state = state.borrow_mut();
+            let mut state = state.lock().unwrap();
             state.update(AppState::LONG);
             gui.update_from(&state);
             Inhibit(false)
@@ -59,7 +58,7 @@ fn main() {
         let gui = Arc::clone(&gui);
         let state = Arc::clone(&state);
         button.connect_enter_notify_event(move |_, _| {
-            let mut state = state.borrow_mut();
+            let mut state = state.lock().unwrap();
             state.update(AppState::DOUBLE);
             gui.update_from(&state);
             Inhibit(false)
--- src/mouse.rs
+++ src/mouse.rs
@@ -1,21 +1,19 @@
 extern crate enigo;
 use crate::app_state::{AppState, State};
 use enigo::{Enigo, MouseButton, MouseControllable};
-use std::cell::RefCell;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-pub fn make_mouse_events(state: Arc<RefCell<State>>) {
+pub fn make_mouse_events(state: Arc<Mutex<State>>) {
     let state = Arc::clone(&state);
     thread::spawn(move || loop {
-        let mut state = state.borrow_mut();
         let wait_time = Duration::from_secs(5);
         let mut enigo = Enigo::new();
 
         thread::sleep(wait_time);
 
-        let button_type = match state.value {
+        let button_type = match state.lock().unwrap().value {
             AppState::DROIT => MouseButton::Right,
             _ => MouseButton::Left,
         };
