4f43e14532eada3da1ea05852ebe221f0fd2b49a
bnau <naubertrand@gmail.com>
2019-11-05 17:56:54 +0100

    Warnings

--- src/main.rs
+++ src/main.rs
@@ -57,9 +57,9 @@ fn main() {
     }
     {
         let button = &gui.button_double.button;
+        let gui = Arc::clone(&gui);
+        let state = Arc::clone(&state);
         button.connect_enter_notify_event(move |_, _| {
-            let gui = Arc::clone(&gui);
-            let state = Arc::clone(&state);
             let mut state = state.lock().unwrap();
             state.update(MouseAction::DOUBLE);
             gui.update(&state);
@@ -78,5 +78,3 @@ fn main() {
     gui.start();
     gtk::main();
 }
-
-fn update_state(state: State, gui: Gui) {}
